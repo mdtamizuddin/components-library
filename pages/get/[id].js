4030e7654ef337725d66df8689dcafd53f75ce78
mdtamizuddin <tamizrabbi@gmail.com>
2022-10-14 20:18:31 +0600

    test

--- pages/get/[id].js
+++ pages/get/[id].js
@@ -1,22 +1,80 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import Navbar from "../../components/Bars/Navbar";
+import Sidebar from "../../components/Bars/Sidebars";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { oneDark } from "@codemirror/theme-one-dark";
 
 const GetCode = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const { id } = router.query;
-  const [codeData, setCodeDat] = useState();
+  const [codeData, setCodeDat] = useState({});
   useEffect(() => {
+    setLoading(true);
     fetch(`http://localhost:3000/api/components/${id}`)
-      .then((res) => res.json())
-      .then((data) => setCodeDat(data));
+      .then((res) => {
+        setLoading(false);
+        return res.json();
+      })
+      .then((data) => {
+        setCodeDat(data);
+      });
   }, [id]);
-  console.log(codeData);
+
+  if (loading) {
+    return "Loading";
+  }
   return (
-    <div>
+    <div className="home-container">
       <Head>
         <title>Component {id}</title>
       </Head>
+      <Sidebar show={showSidebar} setShow={setShowSidebar} />
+      <section className="container-right pb-10 lg:px-5">
+        <Navbar show={showSidebar} setShow={setShowSidebar} />
+        <div className={`${!showSidebar && "h-[100px]"}`}></div>
+
+        <div className="content shadow p-3">
+          <img src={codeData.img} alt="" />
+          <p className=" p-3 mb-3">{codeData.desc}</p>
+          <section
+            className={`${
+              codeData.css && "grid grid-cols-1 lg:grid-cols-2 ga-10"
+            }`}
+          >
+            <div className="p-3">
+              <h1 className="font-bold text-xl mb-3">Code Html / JSX</h1>
+              <CodeMirror
+                value={codeData.code}
+                height="auto"
+                theme={oneDark}
+                extensions={[javascript({ jsx: true })]}
+                onChange={(value, viewUpdate) => {
+                  console.log("value:", value);
+                }}
+              />
+            </div>
+            {codeData.css && (
+              <div className="p-3">
+                <h1 className="font-bold text-xl mb-3">Code Style Sheet</h1>
+                <CodeMirror
+                  value={codeData.css}
+                  height="auto"
+                  theme={oneDark}
+                  extensions={[javascript({ jsx: true })]}
+                  onChange={(value, viewUpdate) => {
+                    console.log("value:", value);
+                  }}
+                />
+              </div>
+            )}
+          </section>
+        </div>
+      </section>
     </div>
   );
 };
--- pages/navbars/index.js
+++ pages/navbars/index.js
@@ -6,7 +6,7 @@ import Navbar from '../../components/Bars/Navbar'
 import Addcomponent from "../../components/Form/Addcomponent";
 const Index = ({ data }) => {
   const [showSidebar, setShowSidebar] = useState(true);
-  const [show , setShow] = useState(true)
+  const [show , setShow] = useState(false)
   return (
     <main className="home-container ">
       <Head>
@@ -26,7 +26,7 @@ const Index = ({ data }) => {
         {data.map((nav) => (
           <div key={nav._id} className="p-5 shadow mt-5">
             <h1 className="text-2xl font-bold text-primary mb-3">Navbar</h1>
-            <img className="min-h-[350px] mx-auto max-h-[500px]" src={nav.img} />
+            <img className="mx-auto max-h-[500px]" src={nav.img} />
             <p className="mt-4 text-xl">{"Description"}</p>
             <Link href={`/get/${nav._id}`}>
               <button className="btn btn-primary btn-sm mt-4">Get Code</button>
